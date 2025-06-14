5ad719953a2c3092eaf4b4a16640976ea16e9138
Rabbit-Fur <mai.marcel.gpt@gmail.com>
2025-06-14 17:45:36 +0200

    Add access control to leaderboard

--- web/routes/leaderboard_routes.py
+++ web/routes/leaderboard_routes.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
 
+from web.auth.decorators import r3_required
+
 leaderboard_bp = Blueprint("leaderboard", __name__)
 
+
+@r3_required
 @leaderboard_bp.route("/")
 def leaderboards():
     """Komplette Leaderboard-Übersicht."""
