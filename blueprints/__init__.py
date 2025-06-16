9afdf7b7066170beba7d3e92b72b133c42f6a68d
Rabbit-Fur <mai.marcel.gpt@gmail.com>
2025-06-16 07:47:14 +0200

    Update __init__.py

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,5 +1,9 @@
+# blueprints/__init__.py
+
 from .api_events import api_events
 from .api_users import api_users
-
-app.register_blueprint(api_events)
-app.register_blueprint(api_users)
+from .admin import admin
+from .public import public
+# ... weitere Blueprints, z. B.:
+# from .leaderboard import leaderboard
+# from .reminder import reminder
